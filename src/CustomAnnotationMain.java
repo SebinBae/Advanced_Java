0fe7a9e34a975b9afb26bb0e050424653d4b0b6c
BaeSeBin <143635021+SebinBae@users.noreply.github.com>
2025-08-26 20:27:40 +0900

    Custom Annotation : 실제 작업전 기본

---
+++ src/CustomAnnotationMain.java
@@ -0,0 +1,9 @@
+public class CustomAnnotationMain {
+
+    public static void main(String[] args){
+
+        Car car = new Car("Chevrolet orlando", 2021);
+        System.out.println(car);
+    }
+
+}
